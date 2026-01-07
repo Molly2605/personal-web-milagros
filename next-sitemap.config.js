f01541f0ecbbcfcf78021241807b24c512937baa
Molly <mili_lasschar@hotmail.com>
2026-01-07 00:40:11 -0300

    change json sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://milagros-lasschar-portfolio.netlify.app',
-  generateRobotsTxt: true,
-  changefreq: 'monthly',
-  priority: 0.7,
-  sitemapSize: 5000,
+  generateRobotsTxt: true, // genera robots.txt automáticamente
+  changefreq: 'monthly',   // ctiempo carga de pagina
+  priority: 0.7,           // prioridad general
+  sitemapSize: 5000,       // cantidad máxima de URLs por sitemap
+  exclude: [],             // si hay páginas que NO quiero incluir, van acá
 }
